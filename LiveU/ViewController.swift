9134e9b2aa2e1323f4f8bf815ecc1a08daa40071
Eric Sharkey <eisharkey@fullsail.student.edu>
2018-09-04 21:19:50 -0400

    Initial Commit

---
+++ LiveU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveU
+//
+//  Created by Eric Sharkey on 9/4/18.
+//  Copyright © 2018 Eric Sharkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
